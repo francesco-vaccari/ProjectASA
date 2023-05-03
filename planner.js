0ce2547771b2fa79def76f17d8426fd1c272e417
francesco-vaccari <francesco.vaccari@studenti.unitn.it>
2023-05-03 23:14:54 +0200

    improved heuristics

--- planner.js
+++ planner.js
@@ -43,19 +43,21 @@ class Planner{
             
             if(this.verbose){
                 setInterval(() => {
-                    this.print()
-                }, 700)
+                    this.print(agent)
+                    // console.log(this.plan)
+                }, 100)
             }
         }, 900)
     }
 
     normalCellHeuristic(x, y, map, agent, parcelsManager, agentsManager){
-        let minDistanceToBorder = 100000
+        let minDistanceToBorder = 10000000
         let borderx = undefined
         let bordery = undefined
         for (let i = 0; i < this.n_rows; i++){
             for(let j = 0; j < this.n_cols; j++){
                 if(this.map[i][j] === 2){
+                    // let tempDist = BFS(x, y, i, j, map).length
                     let tempDist = ManhattanDistance(x, y, i, j)
                     if(tempDist < minDistanceToBorder){
                         minDistanceToBorder = tempDist
@@ -67,13 +69,32 @@ class Planner{
         }
 
         let parcelsRewardInCell = 0
+        let parcelAndAgentInThisCell = false
         for (const parcel of parcelsManager.parcels.elements){
             if(parcel[1].x == x && parcel[1].y == y && parcel[1].carriedBy == null){
+                if(parcel[1].x == agent.x && parcel[1].y == agent.y){
+                    parcelAndAgentInThisCell = true
+                    parcelsRewardInCell += 1000000
+                }
                 parcelsRewardInCell += parcel[1].reward
             }
         }
+
+        let distanceToAgent = 0
+        if(parcelsRewardInCell != 0){
+            // distanceToAgent = ManhattanDistance(x, y, agent.x, agent.y)
+            distanceToAgent = BFS(x, y, agent.x, agent.y, map).length
+        }
+
+
+        // let enemiesProximity = 0
+        // let enemiesProximityCoeff = 3
+        // for (const agent of agentsManager.agents.elements){
+        //     let distanceToEnemy = ManhattanDistance(x, y, agent[1].x, agent[1].y)
+        //     enemiesProximity += Math.pow((((1/(distanceToEnemy+1) - 1)) * enemiesProximityCoeff), 2)
+        // }
         
-        return minDistanceToBorder + parcelsRewardInCell + 1
+        return Math.pow(parcelsRewardInCell, 1.2) + minDistanceToBorder/1000 - Math.pow(distanceToAgent, 2)
     }
 
     deliveryCellHeuristic(x, y, map, agent, parcelsManager, agentsManager){
@@ -84,10 +105,12 @@ class Planner{
             }
         }
 
-        let distanceToAgent = BFS(x, y, agent.x, agent.y, map).length
+        let distanceToAgent = 0
+        if(scoreParcelsCarriedByAgent != 0){
+            distanceToAgent = BFS(x, y, agent.x, agent.y, map).length
+        }
 
-        
-        return scoreParcelsCarriedByAgent - distanceToAgent
+        return Math.pow(scoreParcelsCarriedByAgent, 0.8) - Math.pow(distanceToAgent, 1.2)
     }
 
     async startPlanning(map, agentsManager, parcelsManager, agent){
@@ -97,7 +120,7 @@ class Planner{
             for (let i = 0; i < this.n_rows; i++){
                 for (let j = 0; j < this.n_cols; j++){
                     if(this.map[i][j] === 0){ // wall cell
-                        this.scoreMap[i][j] = -1000000
+                        this.scoreMap[i][j] = 0
                     } else if(this.map[i][j] === 1){ // normal cell
                         this.scoreMap[i][j] = this.normalCellHeuristic(i, j, map, agent, parcelsManager, agentsManager)
                     } else if(this.map[i][j] === 2){ // delivery cell
@@ -112,7 +135,7 @@ class Planner{
             let intention = undefined
             for (let i = 0; i < this.n_rows; i++){
                 for (let j = 0; j < this.n_cols; j++){
-                    if(this.scoreMap[i][j] > bestscore){
+                    if(this.scoreMap[i][j] > bestscore && this.map[i][j] !== 0){
                         bestscore = this.scoreMap[i][j]
                         target_x = i
                         target_y = j
@@ -126,8 +149,8 @@ class Planner{
             }
 
 
-            
             this.plan = BFS(agent.x, agent.y, target_x, target_y, map)
+            
             /*
             Oggetti che ci sono:
                 - target_x
@@ -147,22 +170,49 @@ class Planner{
         return this.plan
     }
 
-    print(){
+    print(agent){
         console.log('\n-----[SCOREMAP]-----')
         let out = ''
-        for (let col = this.n_cols-1; col >= 0; col--){
-            for (let row = 0; row < this.n_rows; row++){
-                if(this.scoreMap[row][col] == -1000000){
-                    out += '  '
-                } else {
-                    out += this.scoreMap[row][col] + ' '
+        let padding = 4
+        let highestValue = -100000000
+        let lowestValue = 100000000
+        for (let i = 0; i < this.n_rows; i++){
+            for (let j = 0; j < this.n_cols; j++){
+                if(this.scoreMap[i][j] > highestValue && this.map[i][j] !== 0){
+                    highestValue = this.scoreMap[i][j]
                 }
             }
-            out += '\n'
+        }
+        for (let i = 0; i < this.n_rows; i++){
+            for (let j = 0; j < this.n_cols; j++){
+                if(this.scoreMap[i][j] < lowestValue && this.map[i][j] !== 0){
+                    lowestValue = this.scoreMap[i][j]
+                }
+            }
+        }
+        for (let col = this.n_cols-1; col >= 0; col--){
+            for (let row = 0; row < this.n_rows; row++){
+                if(agent.x == row && agent.y == col){
+                    out += addPadding('AGN', padding)
+                } else if(this.map[row][col] == 0){
+                    out += addPadding('', padding)
+                } else {
+                    out += addPadding(Math.round(100*(this.scoreMap[row][col]-lowestValue)/(highestValue-lowestValue)), padding)
+                }
+            }
+            out += '\n\n'
         }
         console.log(out)
         console.log('-------------------\n')
     }
+}
+
+function addPadding(item, padding){
+    let out = '' + item
+    while(out.length < padding){
+        out += ' '
+    }
+    return out
 }
 
 export { Planner }
