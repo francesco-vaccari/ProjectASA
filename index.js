007774e61f6ae8162d69893f4edb7f6b8d60fc1f
francesco-vaccari <francesco.vaccari@studenti.unitn.it>
2023-05-16 13:03:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,67 +15,67 @@ client.onConfig(data => {
     conf.PARCEL_DECADING_INTERVAL = data.PARCEL_DECADING_INTERVAL
 })
 
-// const agent = new You(client, false)
-// const map = new GameMap(client, false)
-// const parcels = new Parcels(client, false)
-// const agents = new Agents(client, false)
+const agent = new You(client, false)
+const map = new GameMap(client, false)
+const parcels = new Parcels(client, false)
+const agents = new Agents(client, false)
 
-// while(!map.isReady){
-//     await new Promise(res => setImmediate(res))
-// }
+while(!map.isReady){
+    await new Promise(res => setImmediate(res))
+}
 
-// const planner = new Planner(map, agents, parcels, agent, false)
+const planner = new Planner(map, agents, parcels, agent, false)
 
 
 
-// var plan = []
-// var lastAction = undefined
-// var ready = true
+var plan = []
+var lastAction = undefined
+var ready = true
 
-// async function agentControlLoop(){
-//     while(true){
-//         plan = planner.getPlan()
-//         if(ready && plan.length > 0){
-//             // console.log('READY')
-//             ready = false
-//             lastAction = plan[0]
-//             // console.log('\tACTION ' + lastAction)
+async function agentControlLoop(){
+    while(true){
+        plan = planner.getPlan()
+        if(ready && plan.length > 0){
+            // console.log('READY')
+            ready = false
+            lastAction = plan[0]
+            // console.log('\tACTION ' + lastAction)
             
-//             switch (lastAction) {
-//                 case 'up':
-//                 case 'down':
-//                 case 'left':
-//                 case 'right':
-//                     client.move(lastAction).then((res) => {
-//                         // console.log('\tRESULT ' + res)
-//                         ready = true
-//                         parcels.updateUncertainty()
-//                     })
-//                     break;
-//                 case 'pickup':
-//                     await client.pickup().then(() => {
-//                         // console.log('\tDONE')
-//                         ready = true
-//                     })
-//                     break;
-//                 case 'putdown':
-//                     await client.putdown().then(() => {
-//                         // console.log('\tDONE')
-//                         ready = true
-//                         parcels.clearPutdownParcels(agent.id)
-//                     })
-//                     break;
-//                 default:
-//                     // console.log('ERROR')
-//                     ready = true
-//                     break;
-//             }
+            switch (lastAction) {
+                case 'up':
+                case 'down':
+                case 'left':
+                case 'right':
+                    client.move(lastAction).then((res) => {
+                        // console.log('\tRESULT ' + res)
+                        ready = true
+                        parcels.updateUncertainty()
+                    })
+                    break;
+                case 'pickup':
+                    await client.pickup().then(() => {
+                        // console.log('\tDONE')
+                        ready = true
+                    })
+                    break;
+                case 'putdown':
+                    await client.putdown().then(() => {
+                        // console.log('\tDONE')
+                        ready = true
+                        parcels.clearPutdownParcels(agent.id)
+                    })
+                    break;
+                default:
+                    // console.log('ERROR')
+                    ready = true
+                    break;
+            }
 
-//             plan.shift()
-//         }
-//         await new Promise(res => setImmediate(res))
-//     }
-// }
+            plan.shift()
+        }
+        await new Promise(res => setImmediate(res))
+    }
+}
 
-// agentControlLoop()
+agentControlLoop()
 
